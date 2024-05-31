9c89b2e9b451498e9399d79fc68f9363b820b2b3
tahj <tahjamie1@gmail.com>
2024-05-31 19:07:01 -0400

    removed env and changes made

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,38 +1,57 @@
-const {getUsers,getUserById,newUser,updateUser,destroyUser,addItemToCart, removeItemFromCart,getUserByEmail} = require("../models/users"); 
-const jwt = require('jsonwebtoken'); //so i know when users are logged in 
-const bcrypt = require('bcrypt')
-require('dotenv').config()
+const {
+  getUsers,
+  getUserById,
+  newUser,
+  updateUser,
+  destroyUser,
+  addItemToCart,
+  removeItemFromCart,
+  getUserByEmail,
+} = require("../models/users");
+const jwt = require("jsonwebtoken"); //so i know when users are logged in
+const bcrypt = require("bcrypt");
+require("dotenv").config();
 
 const getAllUsers = async (req, res) => {
   try {
     const result = await getUsers();
     res.json(result);
-  }  catch (Error) {
-        res.json({ Error });
+  } catch (error) {
+    console.log(error)
+    res.status(500).send();
   }
 };
 
 const getById = async (req, res) => {
-  const id = req.get('id')
+  const id = req.get("id");
   try {
     const result = await getUserById(id);
     res.json(result);
-  } catch (Error) {
-    res.json({ Error });
+  } catch (error) {
+    console.log(error)
+    res.status(500).send();
   }
 };
 
 const createUser = async (req, res) => {
-  const { first_name, last_name, phone_number, password, email, address } = req.body;
+  const { first_name, last_name, phone_number, password, email, address } =
+    req.body;
   console.log(req.body);
-  const hashPassword = await bcrypt.hash(password, 10)
+  const hashPassword = await bcrypt.hash(password, 10);
   try {
-    const result = await newUser(first_name, last_name, phone_number, hashPassword, email, address);
+    const result = await newUser(
+      first_name,
+      last_name,
+      phone_number,
+      hashPassword,
+      email,
+      address
+    );
     const token = jwt.sign({ email }, process.env.AUTH_KEY);
-    res.json({result, token});
+    res.json({ result, token });
   } catch (error) {
-    console.log(error)
-    res.json({ error });
+    console.log(error);
+    res.status(500).send();
   }
 };
 
@@ -41,71 +60,81 @@ const login = async (req, res) => {
     const { email, password } = req.body;
     const user = await getUserByEmail(email);
 
-    if (!user.id) {  //if user exists 
+    if (!user.id) {
+      //if user exists
       return res.status(401).send("User Does Not Exist.");
     }
     const isValidPassword = await bcrypt.compare(password, user.password);
 
     if (isValidPassword) {
-    const token = jwt.sign({ email : user.email }, process.env.AUTH_KEY);
-    res.json({user, token})
-
+      const token = jwt.sign({ email: user.email }, process.env.AUTH_KEY);
+      res.json({ user, token });
     }
-  } catch (err) {
-    res.status(500).send(err);
+  } catch (error) {
+    console.log(error)
+    res.status(500).send();
   }
 };
-
 
 // const logout = async (req,res) {
 //   res.send({user,token}) = null;
 // }
 
-
 const updateUserById = async (req, res) => {
-  const id = req.get('id')
-  const { first_name, last_name, phone_number, password, email, address } = req.body;
+  const id = req.get("id");
+  const { first_name, last_name, phone_number, password, email, address } =
+    req.body;
   try {
-    const result = await updateUser(id, first_name, last_name, phone_number, password, email, address);
+    const result = await updateUser(
+      id,
+      first_name,
+      last_name,
+      phone_number,
+      password,
+      email,
+      address
+    );
     res.json(result);
-  } catch (Error) {
-    res.json({ Error });
+  } catch (error) {
+    console.log(error)
+    res.status(500).send();
   }
 };
 
 const deleteUser = async (req, res) => {
-  const id = req.get('id')
+  const id = req.get("id");
   try {
     const result = await destroyUser(id);
     res.json(result);
-  } catch (Error) {
-    res.json({ Error });
+  } catch (error) {
+    console.log(error)
+    res.status(500).send();
   }
 };
 
 //addItemToCart
-const addItem = async(req,res) => {
-    const { items, cart_id } = req.body;
-    try {
-      const result = await addItemToCart(items, cart_id);
-      res.json(result);
-    } catch (Error) {
-      res.json({ Error });
-    }
-}
-
+const addItem = async (req, res) => {
+  const { items, cart_id } = req.body;
+  try {
+    const result = await addItemToCart(items, cart_id);
+    res.json(result);
+  } catch (error) {
+    console.log(error)
+    res.status(500).send();
+  }
+};
 
 //removeItemFromCart
-const removeItem = async(req,res) => {
-    const id = req.get('id')
-    try {
-      const result = await removeItemFromCart(id);
-      res.json(result);
-    } catch (Error) {
-      res.json({ Error });
-    }
-}
-
+const removeItem = async (req, res) => {
+  const id = req.get("id");
+  try {
+    const result = await removeItemFromCart(id);
+    res.json(result);
+  } catch (error) {
+    console.log(error)
+    res.status(500).send();
+  }
+};
 
 module.exports = {
   getAllUsers,
@@ -115,5 +144,5 @@ module.exports = {
   deleteUser,
   addItem,
   removeItem,
-  login
+  login,
 };
